88de31c585e11cc5b3d62ae492fafd5a13ac41bb
Milena Famler <m.famler.students.htl-leonding.ac.at>
2026-04-15 16:44:26 +0200

    commented everything out to fix as it takes too long to find mistake in old code yesterday forgot to commit

--- project/js/spritegame_main.js
+++ project/js/spritegame_main.js
@@ -9,33 +9,33 @@
 /***********************************
  * GAME SCREEN
  ***********************************/
-let GAME_SCREEN = {
+/*let GAME_SCREEN = {
     surface: document.getElementById('surface'),
     surfaceScale: '80%',
     redbox: document.getElementById('redBox'),
     startButton: document.getElementById('startButton'),
     debug_output: document.getElementById('debug_output')
-}
+}*/
 
 // Scale the surface to xx% of the screen width
-GAME_SCREEN.surface.style.transform = `scale(${parseFloat(GAME_SCREEN.surfaceScale)/100 * (window.innerWidth / GAME_SCREEN.surface.clientWidth)})`;
+//GAME_SCREEN.surface.style.transform = `scale(${parseFloat(GAME_SCREEN.surfaceScale)/100 * (window.innerWidth / GAME_SCREEN.surface.clientWidth)})`;
 
 
 
 /***********************************
  * GAME CONFIG
  ***********************************/
-let GAME_CONFIG = {
+/*let GAME_CONFIG = {
     gameSpeed: 24, // game loop refresh rate (pictures per second)
     characterSpeed: 5 // move offset in PX
-}
+}*/
 
 
 
 /***********************************
  * START GAME
  * **********************************/
-function startGame() {
+/*function startGame() {
     hom.style.display = "none"
     gam.style.display = "block"
 
@@ -48,22 +48,22 @@ function startGame() {
     GAME_SCREEN.startButton.removeAttribute('onclick');
 
     gameLoop();
-}
+}*/
 
 
 
 /***********************************
  * DRAW HUD (debugger box)
  * **********************************/
-function updateHUD(){
+/*function updateHUD(){
     // print values in debugger box
     GAME_SCREEN.debug_output.innerHTML = `x: ${PLAYER.box.style.left} | y: ${PLAYER.box.style.top} | animation: ${PLAYER.spriteImgNumber} | count: ${PLAYER.coinCount}`;
-}
+}*/
 
 
 
 ///
-let gam = document.getElementById("game")
+/*let gam = document.getElementById("game")
 let hom = document.getElementById("home")
 let rul = document.getElementById("rules")
 let log = document.getElementById("logo")
@@ -95,7 +95,7 @@ function showRules() {
     if(!isOnRules) {
         isOnRules = true
 
-        rul.onclick = ""
+        //rul.onclick = ""
 
         log.style.display = "none"
         sta.style.display = "none"
@@ -129,7 +129,7 @@ function showRules() {
     else if(isOnRules) {
         isOnRules = false
 
-        rul.onclick = showRules
+        //rul.onclick = showRules
 
         txt = ""
         rul.innerHTML = txt
@@ -166,5 +166,5 @@ function showRules() {
     rul.innerHTML = txt
     txt = ""
     rul.style = "width: 40vw; left: 28.5%; background-color: rgba(133, 222, 255, 0.74); padding: 1.5%; height: 44vh; font-size: 120%; border-radius: 60px; cursor: url(img/cursor/closed.png), auto;"
-    */
-}
+    *//*
+}*/
